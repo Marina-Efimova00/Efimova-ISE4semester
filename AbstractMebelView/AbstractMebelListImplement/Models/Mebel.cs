35b33a861c5b67513ec3ea9545a54679db6e558e
Марина Ефимова <marina.efimova.00@bk.ru>
2020-03-19 19:10:27 +0400

    lab1class(done)

---
+++ AbstractMebelView/AbstractMebelListImplement/Models/Mebel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractShopListImplement.Models
+{
+    public class Mebel
+    {
+        public int Id { get; set; }
+        public string MebelName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
