ec03d15e6a6d131fd5f49205b0cd7b0aaf689dd0
Марина Ефимова <marina.efimova.00@bk.ru>
2020-03-20 04:19:09 +0400

    lab3class

--- AbstractMebelView/AbstractMebelDatabaseImplement/Models/Zagotovka.cs
+++ AbstractMebelView/AbstractMebelDatabaseImplement/Models/Zagotovka.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
+
 namespace AbstractMebelDatabaseImplement.Models
 {
     public class Zagotovka
