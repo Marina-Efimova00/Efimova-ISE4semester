22065a320f6ac7bb5f5ce05f5a9af326480b1ef4
Марина Ефимова <marina.efimova.00@bk.ru>
2020-03-20 04:17:11 +0400

    lab3class

--- AbstractMebelView/AbstractMebelFileImplement/Models/MebelZagotovka.cs
+++ AbstractMebelView/AbstractMebelFileImplement/Models/MebelZagotovka.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AbstractMebelListImplement.Models
+namespace AbstractMebelFileImplement.Models
 {
     public class MebelZagotovka
     {
