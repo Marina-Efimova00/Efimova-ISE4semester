46742d8358a31da0e4f77ecff92b46ea1644cc6f
Марина Ефимова <marina.efimova.00@bk.ru>
2020-03-20 13:05:27 +0400

    lab3class(done)

--- AbstractMebelView/AbstractMebelDatabaseImplement/Models/Mebel.cs
+++ AbstractMebelView/AbstractMebelDatabaseImplement/Models/Mebel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace AbstractMebelDatabaseImplement.Models
@@ -15,7 +16,7 @@ namespace AbstractMebelDatabaseImplement.Models
 
         [Required]
         public decimal Price { get; set; }
-
+        [ForeignKey("MebelId")]
         public virtual List<MebelZagotovka> MebelZagotovkas { get; set; }
     }
 }
