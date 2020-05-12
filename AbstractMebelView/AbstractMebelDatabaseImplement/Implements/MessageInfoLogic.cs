1510f55ed38e72b674f5681a9951ee88ed3082ac
Марина Ефимова <marina.efimova.00@bk.ru>
2020-05-13 01:46:54 +0400

    lab7class

---
+++ AbstractMebelView/AbstractMebelDatabaseImplement/Implements/MessageInfoLogic.cs
@@ -0,0 +1,59 @@
+﻿using AbstractMebelBusinessLogic.BindingModels;
+using AbstractMebelBusinessLogic.Interfaces;
+using AbstractMebelBusinessLogic.ViewModels;
+using AbstractMebelDatabaseImplement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AbstractMebelDatabaseImplement.Implements
+{
+    public class MessageInfoLogic : IMessageInfoLogic
+    {
+        public void Create(MessageInfoBindingModel model)
+        {
+            using (var context = new AbstractMebelDatabase())
+            {
+                MessageInfo element = context.MessageInfoes.FirstOrDefault(rec => rec.MessageId == model.MessageId);
+
+                if (element != null)
+                {
+                    throw new Exception("Уже есть письмо с таким идентификатором");
+                }
+
+                int? clientId = context.Clients.FirstOrDefault(rec => rec.Email == model.FromMailAddress)?.Id;
+
+                context.MessageInfoes.Add(new MessageInfo
+                {
+                    MessageId = model.MessageId,
+                    ClientId = clientId,
+                    SenderName = model.FromMailAddress,
+                    DateDelivery = model.DateDelivery,
+                    Subject = model.Subject,
+                    Body = model.Body
+                });
+
+                context.SaveChanges();
+            }
+        }
+
+        public List<MessageInfoViewModel> Read(MessageInfoBindingModel model)
+        {
+            using (var context = new AbstractMebelDatabase())
+            {
+                return context.MessageInfoes
+                .Where(rec => model == null || rec.ClientId == model.ClientId)
+                .Select(rec => new MessageInfoViewModel
+                {
+                    MessageId = rec.MessageId,
+                    SenderName = rec.SenderName,
+                    DateDelivery = rec.DateDelivery,
+                    Subject = rec.Subject,
+                    Body = rec.Body
+                })
+               .ToList();
+            }
+        }
+    }
+}
