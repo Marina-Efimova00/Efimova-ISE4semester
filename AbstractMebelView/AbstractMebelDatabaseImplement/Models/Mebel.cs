4599721a12bcaf2a55545d81cb853cf26ce7207e
Марина Ефимова <marina.efimova.00@bk.ru>
2020-05-21 16:52:59 +0400

    lab6class(done)

--- AbstractMebelView/AbstractMebelDatabaseImplement/Models/Mebel.cs
+++ AbstractMebelView/AbstractMebelDatabaseImplement/Models/Mebel.cs
@@ -16,5 +16,6 @@ namespace AbstractMebelDatabaseImplement.Models
         [Required]
         public decimal Price { get; set; }
         public virtual List<MebelZagotovka> MebelZagotovkas { get; set; }
+        public virtual List<Order> Orders { get; set; }
     }
 }
