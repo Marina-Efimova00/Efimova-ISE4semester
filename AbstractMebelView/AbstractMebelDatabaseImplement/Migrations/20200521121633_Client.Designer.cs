2318af30cb6d069c3d296d337880071abfe900c2
Марина Ефимова <marina.efimova.00@bk.ru>
2020-05-28 13:59:07 +0400

    lab6class(done)

--- AbstractMebelView/AbstractMebelDatabaseImplement/Migrations/20200521121633_Client.Designer.cs
+++ AbstractMebelView/AbstractMebelDatabaseImplement/Migrations/20200521121633_Client.Designer.cs
@@ -22,185 +22,185 @@ namespace AbstractMebelDatabaseImplement.Migrations
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("AbstractMebelDatabaseImplement.Models.Client", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int")
+                    .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<string>("ClientFIO")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                b.Property<string>("ClientFIO")
+                    .IsRequired()
+                    .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("Email")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                b.Property<string>("Email")
+                    .IsRequired()
+                    .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("Password")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                b.Property<string>("Password")
+                    .IsRequired()
+                    .HasColumnType("nvarchar(max)");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.ToTable("Clients");
-                });
+                b.ToTable("Clients");
+            });
 
             modelBuilder.Entity("AbstractMebelDatabaseImplement.Models.Implementer", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int")
+                    .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<string>("ImplementerFIO")
-                        .HasColumnType("nvarchar(max)");
+                b.Property<string>("ImplementerFIO")
+                    .HasColumnType("nvarchar(max)");
 
-                    b.Property<int>("PauseTime")
-                        .HasColumnType("int");
+                b.Property<int>("PauseTime")
+                    .HasColumnType("int");
 
-                    b.Property<int>("WorkingTime")
-                        .HasColumnType("int");
+                b.Property<int>("WorkingTime")
+                    .HasColumnType("int");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.ToTable("Implementers");
-                });
+                b.ToTable("Implementers");
+            });
 
             modelBuilder.Entity("AbstractMebelDatabaseImplement.Models.Mebel", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int")
+                    .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<string>("MebelName")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                b.Property<string>("MebelName")
+                    .IsRequired()
+                    .HasColumnType("nvarchar(max)");
 
-                    b.Property<decimal>("Price")
-                        .HasColumnType("decimal(18,2)");
+                b.Property<decimal>("Price")
+                    .HasColumnType("decimal(18,2)");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.ToTable("Mebels");
-                });
+                b.ToTable("Mebels");
+            });
 
             modelBuilder.Entity("AbstractMebelDatabaseImplement.Models.MebelZagotovka", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int")
+                    .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<int>("Count")
-                        .HasColumnType("int");
+                b.Property<int>("Count")
+                    .HasColumnType("int");
 
-                    b.Property<int>("MebelId")
-                        .HasColumnType("int");
+                b.Property<int>("MebelId")
+                    .HasColumnType("int");
 
-                    b.Property<int>("ZagotovkaId")
-                        .HasColumnType("int");
+                b.Property<int>("ZagotovkaId")
+                    .HasColumnType("int");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.HasIndex("MebelId");
+                b.HasIndex("MebelId");
 
-                    b.HasIndex("ZagotovkaId");
+                b.HasIndex("ZagotovkaId");
 
-                    b.ToTable("MebelZagotovkas");
-                });
+                b.ToTable("MebelZagotovkas");
+            });
 
             modelBuilder.Entity("AbstractMebelDatabaseImplement.Models.Order", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int")
+                    .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<int>("ClientId")
-                        .HasColumnType("int");
+                b.Property<int>("ClientId")
+                    .HasColumnType("int");
 
-                    b.Property<int>("Count")
-                        .HasColumnType("int");
+                b.Property<int>("Count")
+                    .HasColumnType("int");
 
-                    b.Property<DateTime>("DateCreate")
-                        .HasColumnType("datetime2");
+                b.Property<DateTime>("DateCreate")
+                    .HasColumnType("datetime2");
 
-                    b.Property<DateTime?>("DateImplement")
-                        .HasColumnType("datetime2");
+                b.Property<DateTime?>("DateImplement")
+                    .HasColumnType("datetime2");
 
-                    b.Property<int?>("ImplementerId")
-                        .HasColumnType("int");
+                b.Property<int?>("ImplementerId")
+                    .HasColumnType("int");
 
-                    b.Property<int>("MebelId")
-                        .HasColumnType("int");
+                b.Property<int>("MebelId")
+                    .HasColumnType("int");
 
-                    b.Property<int>("Status")
-                        .HasColumnType("int");
+                b.Property<int>("Status")
+                    .HasColumnType("int");
 
-                    b.Property<decimal>("Sum")
-                        .HasColumnType("decimal(18,2)");
+                b.Property<decimal>("Sum")
+                    .HasColumnType("decimal(18,2)");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.HasIndex("ClientId");
+                b.HasIndex("ClientId");
 
-                    b.HasIndex("ImplementerId");
+                b.HasIndex("ImplementerId");
 
-                    b.HasIndex("MebelId");
+                b.HasIndex("MebelId");
 
-                    b.ToTable("Orders");
-                });
+                b.ToTable("Orders");
+            });
 
             modelBuilder.Entity("AbstractMebelDatabaseImplement.Models.Zagotovka", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int")
+                    .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<string>("ZagotovkaName")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                b.Property<string>("ZagotovkaName")
+                    .IsRequired()
+                    .HasColumnType("nvarchar(max)");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.ToTable("Zagotovkas");
-                });
+                b.ToTable("Zagotovkas");
+            });
 
             modelBuilder.Entity("AbstractMebelDatabaseImplement.Models.MebelZagotovka", b =>
-                {
-                    b.HasOne("AbstractMebelDatabaseImplement.Models.Mebel", "Mebel")
-                        .WithMany("MebelZagotovkas")
-                        .HasForeignKey("MebelId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
+            {
+                b.HasOne("AbstractMebelDatabaseImplement.Models.Mebel", "Mebel")
+                    .WithMany("MebelZagotovkas")
+                    .HasForeignKey("MebelId")
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .IsRequired();
 
-                    b.HasOne("AbstractMebelDatabaseImplement.Models.Zagotovka", "Zagotovka")
-                        .WithMany("MebelZagotovkas")
-                        .HasForeignKey("ZagotovkaId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-                });
+                b.HasOne("AbstractMebelDatabaseImplement.Models.Zagotovka", "Zagotovka")
+                    .WithMany("MebelZagotovkas")
+                    .HasForeignKey("ZagotovkaId")
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .IsRequired();
+            });
 
             modelBuilder.Entity("AbstractMebelDatabaseImplement.Models.Order", b =>
-                {
-                    b.HasOne("AbstractMebelDatabaseImplement.Models.Client", "Client")
-                        .WithMany("Orders")
-                        .HasForeignKey("ClientId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
+            {
+                b.HasOne("AbstractMebelDatabaseImplement.Models.Client", "Client")
+                    .WithMany("Orders")
+                    .HasForeignKey("ClientId")
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .IsRequired();
 
-                    b.HasOne("AbstractMebelDatabaseImplement.Models.Implementer", "Implementer")
-                        .WithMany()
-                        .HasForeignKey("ImplementerId");
+                b.HasOne("AbstractMebelDatabaseImplement.Models.Implementer", "Implementer")
+                    .WithMany()
+                    .HasForeignKey("ImplementerId");
 
-                    b.HasOne("AbstractMebelDatabaseImplement.Models.Mebel", "Mebel")
-                        .WithMany("Orders")
-                        .HasForeignKey("MebelId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-                });
+                b.HasOne("AbstractMebelDatabaseImplement.Models.Mebel", "Mebel")
+                    .WithMany("Orders")
+                    .HasForeignKey("MebelId")
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .IsRequired();
+            });
 #pragma warning restore 612, 618
         }
     }
--- AbstractMebelView/AbstractMebelFileImplement/Implements/OrderLogic.cs
+++ AbstractMebelView/AbstractMebelFileImplement/Implements/OrderLogic.cs
@@ -73,8 +73,6 @@ namespace AbstractMebelFileImplement.Implements
                 ImplementerId = rec.ImplementerId,
                 ImplementerFIO = source.Implementers.FirstOrDefault(recC => recC.Id == rec.ImplementerId)?.ImplementerFIO,
                 MebelName = GetMebelName(rec.MebelId),
-                ClientId = rec.ClientId,
-                ClientFIO = source.Clients.FirstOrDefault(recC => recC.Id == rec.ClientId)?.ClientFIO,
                 Count = rec.Count,
                 Sum = rec.Sum,
                 Status = rec.Status,
